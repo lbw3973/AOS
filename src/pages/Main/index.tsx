7469111a1bedd3194f6ac364ac6756210a92bc2c
LeeByoungWook <lbw3973@naver.com>
2024-07-16 22:29:53 +0900

    feat: Bottom tabs 아이콘 적용

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { Tabs } from "@/navigation/Navigation";
+import { SvgXml } from "react-native-svg";
 import Home from "./Home";
 import Intro from "../Auth/Intro";
 import Login from "../Auth/Login";
+import { BottomIcons } from "@/assets/Icons/Main/BottomIcons";
+import { Dimensions } from "react-native";
+
+const { width } = Dimensions.get("window");
+
+export interface IMainPathName {
+  Main: undefined;
+  Home: undefined;
+}
 
 const Main = () => {
   return (
@@ -19,14 +29,55 @@ const Main = () => {
           shadowOffset: { width: 1, height: 1 },
           shadowColor: "#000000",
         },
+        tabBarStyle: { height: (width / 100) * 12, maxHeight: 80 },
+        tabBarLabelStyle: { display: "none" },
         // header: props => Header(props),
       }}
     >
-      <Tabs.Screen name="Home" component={Home} />
-      <Tabs.Screen name="Group" component={Intro} />
-      <Tabs.Screen name="test1" component={Login} options={{ tabBarStyle: { display: "none" } }} />
-      <Tabs.Screen name="test2" component={Login} />
-      <Tabs.Screen name="test3" component={Home} />
+      <Tabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <SvgXml xml={focused ? BottomIcons.Home_Focus : BottomIcons.Home_UnFocus} width="60%" height="60%" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Group"
+        component={Intro}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <SvgXml xml={focused ? BottomIcons.Church_Focus : BottomIcons.Church_UnFocus} width="60%" height="60%" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Post"
+        component={Login}
+        options={{
+          tabBarStyle: { display: "none" },
+          tabBarIcon: () => <SvgXml xml={BottomIcons.Plus} width="60%" height="60%" />,
+        }}
+      />
+      <Tabs.Screen
+        name="DM"
+        component={Login}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <SvgXml xml={focused ? BottomIcons.DM_Focus : BottomIcons.DM_UnFocus} width="60%" height="60%" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Mypage"
+        component={Home}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <SvgXml xml={focused ? BottomIcons.Mypage_Focus : BottomIcons.Mypage_UnFocus} width="60%" height="60%" />
+          ),
+        }}
+      />
     </Tabs.Navigator>
   );
 };
